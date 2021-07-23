9fd6fe94e147fa90d5bbd6dc21f7b125aea6b45a
joselaurindoferreira <87529477+joselaurindoferreira@users.noreply.github.com>
2021-07-23 20:38:41 -0300

    Pasta do curso de js
    Primeiro commit

---
+++ cursojs/aula11/ex008.js
@@ -0,0 +1,8 @@
+//console.log('o console funcionou');
+var vel = 13;
+
+if(vel > 60){
+  console.log(`A velocidade do seu carro é ${vel}, acima de 60, voce foi multado`)
+}else{
+  console.log('continue')
+}
